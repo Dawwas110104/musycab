caabebee0ab2d5159974af21f93aea1985417cd0
IshlahYT <ishlah.kiky@gmail.com>
2023-06-25 17:34:21 +0700

    ain

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <link rel="shortcut icon" href="public/assets/img/favicon.ico">
   <title>Evote | Musycab</title>
 
   <!-- General CSS Files -->
