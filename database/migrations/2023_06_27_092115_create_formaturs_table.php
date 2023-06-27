2a912e71c4e1f6907aa8605db92f77e7a91323b2
Dawwas Inha Ulhaq <dawwas@MacBook-Pro-Dawwas.local>
2023-06-27 16:22:25 +0700

    CRUD Formatur

---
+++ database/migrations/2023_06_27_092115_create_formaturs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormatursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('formaturs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('formaturs');
+    }
+}
