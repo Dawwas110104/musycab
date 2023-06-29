94f2564a699be1da12a8d4199be2cd1c70127b41
kkevinsm <76790044+kkevinsm@users.noreply.github.com>
2023-06-30 00:35:43 +0700

    dawwasashusangeh

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -43,23 +43,23 @@
                 <div class="card-body">
                     <ul class="nav nav-pills" id="myTab3" role="tablist">
                         <li class="nav-item">
-                            <button class="nav-link fade show active" type="button" data-toggle="collapse" data-target="#collapseExample1" aria-expanded="true" id="home-tab3" data-toggle="tab" href="#visi" role="tab"
+                            <button class="nav-link fade show active" type="button" data-toggle="collapse" data-target="#collapseExample1{{$data->id}}" aria-expanded="true" id="home-tab3" data-toggle="tab" href="#visi" role="tab"
                                 aria-controls="home" aria-selected="true">Visi</button>
                         </li>
                         <li class="nav-item">
-                            <button class="nav-link fade show"  type="button" data-toggle="collapse" data-target="#collapseExample2" aria-expanded="false" id="profile-tab3" data-toggle="tab" href="#misi" role="tab"
+                            <button class="nav-link fade show"  type="button" data-toggle="collapse" data-target="#collapseExample2{{$data->id}}" aria-expanded="false" id="profile-tab3" data-toggle="tab" href="#misi" role="tab"
                                 aria-controls="profile" aria-selected="false">Misi</button>
                         </li>
                     </ul>
                     <div class="tab-content " id="myTabContent2">
-                        <div class="collapse" id="collapseExample1" id="visi" role="tabpanel" aria-labelledby="home-tab3">
+                        <div class="collapse" id="collapseExample1{{$data->id}}" role="tabpanel" aria-labelledby="home-tab3">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
                             cillum dolore eu fugiat nulla pariatur.
                         </div>
-                        <div class="collapse" id="collapseExample2" id="misi" role="tabpanel" aria-labelledby="profile-tab3">
+                        <div class="collapse" id="collapseExample2{{$data->id}}" role="tabpanel" aria-labelledby="profile-tab3">
                             Sed sed metus vel lacus hendrerit tempus. Sed efficitur velit tortor, ac efficitur est
                             lobortis quis. Nullam lacinia metus erat, sed fermentum justo rutrum ultrices. Proin quis
                             iaculis tellus. Etiam ac vehicula eros, pharetra consectetur dui.
