d082429c324a4aa7aff08dc5ed106effb77f1b3a
Dawwas Inha Ulhaq <dawwas@192.168.1.36>
2023-06-30 00:14:25 +0700

    Add Vote Unvote

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Formatur;
 
 class GuestController extends Controller
 {
 
     public function index()
     {
+        $datas = Formatur::all();
         return view('guest.index', compact([
-
+            'datas'
         ]));
     }
     }
