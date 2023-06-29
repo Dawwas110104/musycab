04ae561014a131948b727d01be86c23db84ace0f
kkevinsm <76790044+kkevinsm@users.noreply.github.com>
2023-06-29 23:58:33 +0700

    commitsadsadash

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -23,7 +23,7 @@
             <div class="card">
                 <div class="card-body text-center">
                     <div class="gallery gallery-md">
-                        <div class="gallery-item" data-image="../assets/img/news/img03.jpg" data-title="Image 1"></div>
+                        <div class="gallery-item" data-image="../assets/img/newsimg03.jpg" data-title="Image 1"></div>
                     </div>
                     <div class="mb-2">Joko Widodo</div>
                     <button class="btn btn-primary" id="toastr-1">Vote</button>
@@ -32,23 +32,23 @@
                 <div class="card-body">
                     <ul class="nav nav-pills" id="myTab3" role="tablist">
                         <li class="nav-item">
-                            <a class="nav-link active" id="home-tab3" data-toggle="tab" href="#visi" role="tab"
-                                aria-controls="home" aria-selected="true">Visi</a>
+                            <button class="nav-link fade show active" type="button" data-toggle="collapse" data-target="#collapseExample1" aria-expanded="true" id="home-tab3" data-toggle="tab" href="#visi" role="tab"
+                                aria-controls="home" aria-selected="true">Visi</button>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" id="profile-tab3" data-toggle="tab" href="#misi" role="tab"
-                                aria-controls="profile" aria-selected="false">Misi</a>
+                            <button class="nav-link fade show"  type="button" data-toggle="collapse" data-target="#collapseExample2" aria-expanded="false" id="profile-tab3" data-toggle="tab" href="#misi" role="tab"
+                                aria-controls="profile" aria-selected="false">Misi</button>
                         </li>
                     </ul>
-                    <div class="tab-content" id="myTabContent2">
-                        <div class="tab-pane fade show active" id="visi" role="tabpanel" aria-labelledby="home-tab3">
+                    <div class="tab-content " id="myTabContent2">
+                        <div class="collapse" id="collapseExample1" id="visi" role="tabpanel" aria-labelledby="home-tab3">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
                             cillum dolore eu fugiat nulla pariatur.
                         </div>
-                        <div class="tab-pane fade" id="misi" role="tabpanel" aria-labelledby="profile-tab3">
+                        <div class="collapse" id="collapseExample2" id="misi" role="tabpanel" aria-labelledby="profile-tab3">
                             Sed sed metus vel lacus hendrerit tempus. Sed efficitur velit tortor, ac efficitur est
                             lobortis quis. Nullam lacinia metus erat, sed fermentum justo rutrum ultrices. Proin quis
                             iaculis tellus. Etiam ac vehicula eros, pharetra consectetur dui.
